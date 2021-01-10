76a2d364a15f0d8e9093e9060eb90e0cf224ca1b
adit-sisodia <73304152+adit-sisodia@users.noreply.github.com>
2021-01-10 14:28:15 +0530

    xyz

--- src/Redux/action.js
+++
@@ -1,9 +0,0 @@
-const SHOW_LIST = 'SHOW_LIST';
-
-export const updateDetails = () => {
-    return {
-        type: SHOW_LIST
-    }
-}
-
-export default updateDetails;
--- src/Redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-
-import loginReducer from './loginReducer';
-
-const store = createStore(loginReducer)
-
-export default store;
-
